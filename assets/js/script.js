7fca245d2c21e989364248d4cf505cc1542977ea
carlopezzuti <carlopezzuti89@gmail.com>
2024-03-06 17:17:49 +0100

    Milestone 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,47 @@
-const kmPrice = 0.21;
+const kmPrice = .21
+const calculate = document.getElementById('calculate')
+const cancel = document.getElementById('cancel')
 
-let tripLength = document.getElementById("kilometers");
-let clientAge = document.getElementById("age");
+let nameSurname = document.getElementById('name')
+let tripLength = document.getElementById('km')
+let ageSpan = document.getElementById('age-span')
 
-function calcTicket() {
-    if (clientAge.value < 18) {
-        document.getElementById("result").innerHTML = ((((kmPrice * tripLength.value) * 80) / 100).toFixed(2) + '€');
-    }
-    else if (clientAge.value > 65) {
-        document.getElementById("result").innerHTML = ((((kmPrice * tripLength.value) * 60) / 100).toFixed(2) + '€');
-    }
-    else {
-        document.getElementById("result").innerHTML = ((kmPrice * tripLength.value).toFixed(2) + '€');
-    }
+
+calculate.addEventListener('click', ticketPrice)
+
+function vagonNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const clearButton = document.querySelector('#clear-button');
-clearButton.addEventListener('click', function() {
-    document.querySelector('#kilometers').value = '';
-    document.querySelector('#age').value = '';
-    
-    const userMessageDiv = document.querySelector('#result');
-    userMessageDiv.innerHTML = '';
-    userMessageDiv.classList.remove('active');
-});
+function codeCP(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function ticketPrice() {
+    if (ageSpan.value == 'minorenne') {
+        document.getElementById("ticket-price").innerHTML = ((tripLength.value * kmPrice) * 0.8).toFixed(2) + "€";
+        document.getElementById('offer').innerHTML = "Biglietto Ridotto"
+    } else if (ageSpan.value == 'over-65') {
+        document.getElementById("ticket-price").innerHTML = ((tripLength.value * kmPrice) * 0.6).toFixed(2) + "€";
+        document.getElementById('offer').innerHTML = "Biglietto Senior"
+    }
+    else {
+        document.getElementById("ticket-price").innerHTML = (tripLength.value * kmPrice).toFixed(2) + "€";
+        document.getElementById('offer').innerHTML = "Biglietto Standard"
+    }
+
+    document.getElementById('vagon').innerHTML = vagonNumber(1, 9)
+
+    document.getElementById('cp-code').innerHTML = codeCP(10000, 99999)
+
+    document.getElementById('passenger-name').innerHTML = nameSurname.value
+
+    document.getElementById('ticket').style.display = 'block'
+}
+
+function refreshPage() {
+    window.location.reload();
+}
+
+cancel.addEventListener('click', refreshPage)
+
